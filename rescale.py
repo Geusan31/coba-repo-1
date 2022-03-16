ee23de69da93c18c28bdcfcc407f2a4b01af3da5
Raissan05 <101711336+Raissan05@users.noreply.github.com>
2022-03-16 22:11:25 +0700

    membuat rescale menggunakan python

---
+++ rescale.py
@@ -0,0 +1,37 @@
+import imghdr
+from turtle import width
+import cv2 as cv
+from cv2 import resize
+
+# img = cv.imread('Resources/Photos/cat.jpg')
+# cv.imshow('Cat', img)
+
+def rescaleFrame(frame, scale=0.75):
+    # Images, Videos and Live Video
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width, height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+def changeRes(width,height):
+    # Live Video
+    capture.set(3,width)
+    capture.set(4,height)
+
+# Reading Videos
+capture = cv.VideoCapture('Resources/Videos/dog.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+
+    frame_rezised = rescaleFrame(frame, scale=.2)
+
+    cv.imshow('Video', frame)
+    cv.imshow('Video Rezised', frame_rezised)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
